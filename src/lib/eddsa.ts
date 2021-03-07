152dc652244374315071bf2c7c7865a8ba99e420
0xBeaver <0xBeaver@protonmail.com>
2021-03-08 04:26:36 +0900

    add signWithdrawal func

--- src/lib/eddsa.ts
+++ src/lib/eddsa.ts
@@ -3,17 +3,31 @@ import path from 'path';
 
 import { eddsa } from 'circomlib';
 import { BigNumber, BigNumberish } from 'ethers';
+import { defaultAbiCoder, keccak256 } from 'ethers/lib/utils';
 import { groth16 } from 'snarkjs';
 
 import eddsaVK from '../snarkfiles/eddsa.vk.json';
 
 import {
+  PRIME_Q,
   privToBuffer,
   privToPubKey,
   Proof,
   proofToSnarkjsProof,
   snarkjsProofToContractArg,
 } from './utils';
+
+export const signWithdrawal = async (
+  noteHash: BigNumberish,
+  to: BigNumberish,
+  privKey: BigNumberish
+): Promise<Proof> => {
+  const msg = BigNumber.from(
+    keccak256(defaultAbiCoder.encode(['uint256', 'address'], [noteHash, to]))
+  ).mod(BigNumber.from(PRIME_Q));
+  const proof = await signEdDSA(msg, privKey);
+  return proof;
+};
 
 export const signEdDSA = async (
   message: BigNumberish,
