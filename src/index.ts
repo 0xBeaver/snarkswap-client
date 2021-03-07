80fe2a76181cebe4a6db9c0d28833f67e8f26955
0xBeaver <0xBeaver@protonmail.com>
2021-03-07 23:09:46 +0900

    update export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import circomlib from 'circomlib';
-import snarkjs from 'snarkjs';
-
 import * as pow from './lib/pow';
 import * as eddsa from './lib/eddsa';
-const lib = { circomlib, snarkjs };
-export { lib, pow, eddsa };
+import * as utils from './lib/utils';
+
+export { pow, eddsa, utils };
