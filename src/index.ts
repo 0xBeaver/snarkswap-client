83dccba2559a1e5973ff3650ae493fa40b327161
0xBeaver <0xBeaver@protonmail.com>
2021-03-09 01:48:19 +0900

    export swap functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as eddsa from './lib/eddsa';
 import * as pow from './lib/pow';
+import * as swap from './lib/swap';
 import * as utils from './lib/utils';
 export * from './lib/note';
 
-export { eddsa, pow, utils };
+export { eddsa, pow, swap, utils };
